c9252f338c536b05767a6b243837b151cb7548b8
wiktfic226 <wiktoriafica00@gmail.com>
2022-07-05 10:11:06 +0200

    feat: init project

---
+++ src/main/java/pl/fis/springlbd/SpringLbdApplication.java
@@ -0,0 +1,13 @@
+package pl.fis.springlbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fis/springlbd/SpringLbdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fis.springlbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
