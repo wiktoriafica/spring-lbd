e1d35e8cb1bed143f95c68f4db770ecff9268044
wiktfic226 <wiktoriafica00@gmail.com>
2022-07-08 19:04:01 +0200

    fix: service aspect

--- src/main/java/pl/fis/springlbdday2/aspect/ServiceAspect.java
+++ src/main/java/pl/fis/springlbdday2/aspect/ServiceAspect.java
@@ -5,25 +5,27 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class ServiceAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(ServiceAspect.class);
 
     @Around("execution(* pl.fis.springlbdday2.service..*.*(..))")
-    public void logCallingServiceMethods(ProceedingJoinPoint joinPoint) {
+    public Object logCallingServiceMethods(ProceedingJoinPoint joinPoint) {
         Object[] arguments = joinPoint.getArgs();
+        Object value = joinPoint.getArgs()[0];
         try {
             LOGGER.info("Passed arguments:");
             for(Object argument : arguments)
               LOGGER.info("Argument: " + argument);
-            Object returnedValue = joinPoint.proceed();
-            if(returnedValue != null)
-                LOGGER.info("Returned value: " + returnedValue);
-            else
-                LOGGER.info("Method does not return any value");
+            value = joinPoint.proceed();
+            LOGGER.info("Returned value: " + value);
+            return value;
         } catch(Throwable exception) {
             LOGGER.info("Error occurred while proceeding the service method.");
+            return value;
         }
     }
 }
